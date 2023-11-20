9bce643c48fcd2b94a3b173c6c0a361d4deae945
SHANE <sam6091260@gmail.com>
2023-11-20 11:57:43 +0800

    remove AJAX

--- all.js
+++ all.js
@@ -1,24 +1,43 @@
-// 使用 Axios 請求數據
-const apiPath =
-  "https://raw.githubusercontent.com/hexschool/js-training/main/travelApi.json";
-
-axios
-  .get(apiPath)
-  .then((res) => {
-    if (res.data && res.data.data) {
-      init(res.data.data);
-      initialCardCount = res.data.data.length;
-      updateInitialCardCount();
-    } else {
-      console.error("Invalid data structure:", res.data);
-    }
-  })
-  .catch((error) => {
-    console.error("Error fetching data:", error);
-  });
+let data = [
+  {
+    id: 0,
+    name: "肥宅心碎賞櫻3日",
+    imgUrl:
+      "https://images.unsplash.com/photo-1522383225653-ed111181a951?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1655&q=80",
+    area: "高雄",
+    description: "賞櫻花最佳去處。肥宅不得不去的超讚景點！",
+    group: 87,
+    price: 1400,
+    rate: 10,
+  },
+  {
+    id: 1,
+    name: "貓空纜車雙程票",
+    imgUrl:
+      "https://images.unsplash.com/photo-1501393152198-34b240415948?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80",
+    area: "台北",
+    description:
+      "乘坐以透明強化玻璃為地板的「貓纜之眼」水晶車廂，享受騰雲駕霧遨遊天際之感",
+    group: 99,
+    price: 240,
+    rate: 2,
+  },
+  {
+    id: 2,
+    name: "台中谷關溫泉會1日",
+    imgUrl:
+      "https://images.unsplash.com/photo-1535530992830-e25d07cfa780?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80",
+    area: "台中",
+    description:
+      "全館客房均提供谷關無色無味之優質碳酸原湯，並取用八仙山之山冷泉供蒞臨貴賓沐浴及飲水使用。",
+    group: 20,
+    price: 1765,
+    rate: 7,
+  },
+];
 
 // 遍歷 data資料渲染 card
-function init(data) {
+function init() {
   const cards = document.querySelector(".cards");
 
   let card = "";
@@ -57,6 +76,7 @@ function init(data) {
   });
   cards.innerHTML = card;
 }
+init();
 
 // 監聽 seclect選擇地區則顯示該地區card
 const selectElement = document.querySelector("#select-search");
@@ -82,12 +102,9 @@ selectElement.addEventListener("change", () => {
 });
 
 // 初始卡片筆數
-let initialCardCount = 0;
-
-function updateInitialCardCount() {
-  const searchResult = document.querySelector("#search-result");
-  searchResult.textContent = `本次搜尋共 ${initialCardCount} 筆資料`;
-}
+const initialCardCount = data.length;
+const searchResult = document.querySelector("#search-result");
+searchResult.textContent = `本次搜尋共 ${initialCardCount} 筆資料`;
 
 // // 新增 card
 const ticketName = document.querySelector(".ticket-name");
